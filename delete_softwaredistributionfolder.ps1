1e82313e8dea9feac019cf019cdd69b64351f9e7
broken-overcast <tobizes@gmail.com>
2025-04-28 23:40:02 -0500

    Add delete_softwaredistributionfolder.ps1

---
+++ delete_softwaredistributionfolder.ps1
@@ -0,0 +1,29 @@
+# stop related update services
+Stop-Service -Name "wuauserv", "cryptsvc", "bits", "msiserver"
+
+# grabs all subfolders and contents
+$items Get-ChildItem -Path "C:\Windows\SoftwareDistribution" -recurse -force
+
+# parallels taking ownership of folders and files
+$jobs=@()
+foreach ($item in items) {
+    $jobs += Start-Job -ScriptBlock {
+        param ($path)
+        & takeown /f $path /r /d y
+    } -ArgumentList $item.fullname
+}
+
+# wait for jobs to complete
+$jobs | Wait-Job
+
+# grants admins full control
+icacls "C:\Windows\SoftwareDistribution" /grant administrators: F /T /C
+
+# deletes target folder
+cmd /c rmdir /s /q "C:\Windows\SoftwareDistribution"
+
+# starts services previously stopped
+Start-Service -Name "wuauserv","cryptsvc","bits","msiserver"
+
+# outputs target service status
+Get-Service -Name "wuauserv","cryptsvc","bits","msiserver"
